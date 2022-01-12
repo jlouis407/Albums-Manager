96ae6d1f8bdb5b1a6b4da87d01694c55969b0c67
Josiah Louis <josiah.israel.louis@gmail.com>
2022-01-11 23:48:47 -0500

    Update Album Service, Controller, and Views

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.model.Album;
 import com.example.demo.services.AlbumService;
@@ -16,12 +20,48 @@ public class AppController {
 	@Autowired
 	AlbumService albumService;
 	
-	@GetMapping("/albums")
-	public String viewAlbums(Model model) {
-		List<Album> listStudent = albumService.listAllAlbums();
-		model.addAttribute("listStudent",listStudent);
-		return "albumsList";
+	@RequestMapping("/")
+	public String viewHome(Model model) {
+		List<Album> listAlbums = albumService.listAll();
+		model.addAttribute("listAlbums", listAlbums);
+		
+		return "index";
 	}
+	
+	@RequestMapping("/new")
+	public String viewCreateAlbum(Model model) {
+		Album album = new Album();
+		model.addAttribute("album", album);
+		
+		return "newAlbum";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveAlbum(@ModelAttribute("album") Album album) {
+		albumService.save(album);
+		
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/edit/{id}")
+	public ModelAndView viewEditAlbum(@PathVariable(name = "id") Long id) {
+		ModelAndView mav = new ModelAndView("editAlbum");
+		
+		Album album = albumService.get(id);
+		mav.addObject("album", album);
+		
+		return mav;
+	}	
+	
+	@RequestMapping("/delete/{id}")
+	public String deleteAlbum(@PathVariable(name = "id") Long id) {
+		albumService.delete(id);
+		
+		return "redirect:/";
+	}
+	
+	
+	
 	
 
 }
--- src/main/java/com/example/demo/services/AlbumService.java
+++ src/main/java/com/example/demo/services/AlbumService.java
@@ -3,12 +3,7 @@ package com.example.demo.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Album;
 import com.example.demo.repository.AlbumRepository;
 
@@ -18,30 +13,20 @@ public class AlbumService {
 	@Autowired
 	private AlbumRepository albumRepository;
 	
-	public List<Album> listAllAlbums(){
+	public List<Album> listAll() {		
 		return albumRepository.findAll();
 	}
 	
-	public Album getAlbumById(Long id) {
+	public void save(Album album) {
+		albumRepository.save(album);
+	}
+	
+	public Album get(Long id) {
 		return albumRepository.findById(id).get();
 	}
 	
-	public Album addAlbum(@RequestBody Album album) {
-		return albumRepository.save(album);
+	public void delete(Long id) {
+		albumRepository.deleteById(id);
 	}
-	
-	public ResponseEntity<Album> updateAlbum(@PathVariable Long id, @RequestBody Album albumDetails){
-			
-			Album album = albumRepository.findById(id)
-					.orElseThrow(() -> new ResourceNotFoundException("Album with id: " + id + " does not exist."));
-			
-			album.setTitle(albumDetails.getTitle());
-			album.setArtist(albumDetails.getArtist());
-			album.setGenre(albumDetails.getGenre());
-			album.setYear(albumDetails.getYear());
-			
-			Album updatedAlbum = albumRepository.save(album);
-			return ResponseEntity.ok(updatedAlbum);
-		}
 
 }
